d32df4033d31c4d6f708cb09a66c511c99ab0afc
cthanavat <ch.thanavat@gmail.com>
2025-05-03 19:40:35 +0700

    modal css 2

--- app/page.tsx
+++ app/page.tsx
@@ -445,7 +445,7 @@ export default function HomePage() {
             </div>
             <button
               type="submit"
-              className="w-full rounded-md bg-black py-2 text-sm font-medium text-white hover:opacity-90"
+              className="w-full rounded-md bg-black py-2 text-sm font-medium text-white hover:bg-red-800 hover:opacity-90"
             >
               Submit
             </button>
